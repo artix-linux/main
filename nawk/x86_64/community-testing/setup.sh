4d8f9f385a5e701a5eb024a8563c4725bbe3ee39
Carlos Torres <torr@artixlinux.org>
2022-08-01 22:01:28 -0300

    [trunk] -> [community-testing] 'nawk-20220122-1.2' add

---
+++ nawk/x86_64/community-testing/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+post_upgrade(){
+	test ! -f /usr/bin/awk && \
+		ln -s /usr/bin/nawk /usr/bin/awk
+	test ! -f /usr/share/man/man1/awk.1 && \
+		ln -s /usr/share/man/man1/nawk.1 /usr/share/man/man1/awk.1
+}
+
+pre_remove(){
+	file /usr/bin/awk | grep -q "symbolic link to nawk" && \
+		rm /usr/bin/awk
+	file /usr/share/man/man1/awk.1 | grep -q "symbolic link to nawk" && \
+		rm /usr/share/man/man1/awk.1   
+}
